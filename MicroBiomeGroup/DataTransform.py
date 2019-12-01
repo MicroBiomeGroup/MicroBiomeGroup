9067dfd9bd7b472b014b00df56ffbc5c8ab40451
Kylie Davidson <david9km@dukes.jmu.edu>
2019-12-01 17:25:30 -0500

    Pushing Soumyas code

--- MicroBiomeGroup/DataTransform.py
+++ MicroBiomeGroup/DataTransform.py
@@ -26,7 +26,7 @@ with open('HeatMapBehavioralSeizures.csv', mode='w') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     employee_writer.writerow(['group', 'variable', 'value', 'treatment'])
-    with open("HeatmapData.csv") as f:
+    with open("HeatmapSorted.csv") as f:
         f.readline() #eat the csv header from the lines
         for line in f:
             lineSplit = line.split(',')
@@ -36,6 +36,51 @@ with open('HeatMapBehavioralSeizures.csv', mode='w') as employee_file:
             i = 4 #offset of first behavioral seizure time stamp
             hour = 1
             while(i < 16):
+                value = int(lineSplit[i])
+                time =  "H" + str(hour)
+                hour+=1
+                i+=1
+                treatment = lineSplit[3]
+                employee_writer.writerow([time, key, value, treatment])
+
+
+with open('HeatMapFocalSeizures.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow(['group', 'variable', 'value', 'treatment'])
+    with open("HeatmapSorted.csv") as f:
+        f.readline() #eat the csv header from the lines
+        for line in f:
+            lineSplit = line.split(',')
+            print(lineSplit)
+
+            key = lineSplit[2] + " " +lineSplit[0]
+            i = 16 #offset of first behavioral seizure time stamp
+            hour = 1
+            while(i < 28):
+                value = int(lineSplit[i])
+                time =  "H" + str(hour)
+                hour+=1
+                i+=1
+                treatment = lineSplit[3]
+                employee_writer.writerow([time, key, value, treatment])
+
+
+
+with open('HeatMapCombinedSeizures.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow(['group', 'variable', 'value', 'treatment'])
+    with open("HeatmapSorted.csv") as f:
+        f.readline() #eat the csv header from the lines
+        for line in f:
+            lineSplit = line.split(',')
+            print(lineSplit)
+
+            key = lineSplit[2] + " " +lineSplit[0]
+            i = 28 #offset of first behavioral seizure time stamp
+            hour = 1
+            while(i < 40):
                 value = int(lineSplit[i])
                 time =  "H" + str(hour)
                 hour+=1
